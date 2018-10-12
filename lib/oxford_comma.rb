ee6a4ad72a14341b08e77138a1aa21b70ecc8f34
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-12 18:16:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,3 @@
 def oxford_comma(array)
-  puts array[0..(array.length - 2)].join(", ") + " and " + array[array.length - 1]
+  puts "#{array[0..(array.length - 2)].join(", ")} and #{array[array.length - 1]}"
 end
